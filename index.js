95af1646d51b5e7a58593931bea208b7b434f67a
Federico Brigante <bfred-it@users.noreply.github.com>
2017-06-02 02:51:24 +0800

    Use requestAnimationFrame for FUOC-less resolution (#5)

--- index.js
+++ index.js
@@ -8,25 +8,17 @@ module.exports = selector => {
 	}
 
 	const promise = new Promise(resolve => {
-		const el = document.querySelector(selector);
-
-		// Shortcut if the element already exists
-		if (el) {
-			resolve(el);
-			selectorCache.delete(selector);
-			return;
-		}
-
 		// Interval to keep checking for it to come into the DOM
-		const awaitElement = setInterval(() => {
+		(function check() {
 			const el = document.querySelector(selector);
 
 			if (el) {
 				resolve(el);
-				clearInterval(awaitElement);
 				selectorCache.delete(selector);
+			} else {
+				requestAnimationFrame(check);
 			}
-		}, 50);
+		})();
 	});
 
 	selectorCache.set(selector, promise);
