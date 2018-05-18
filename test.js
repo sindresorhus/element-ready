838a7a2316a3d1cdc3ed4d4ec31490702c73c7da
Sindre Sorhus <sindresorhus@gmail.com>
2018-05-18 23:10:29 +0700

    Update test.js

--- test.js
+++ test.js
@@ -114,12 +114,10 @@ test('ensure different promises are returned on second call with the same select
 	};
 
 	t.is(prependElement(), await m('.unicorn'));
-
+	
 	document.querySelector('.unicorn').remove();
-
 	t.is(prependElement(), await m('.unicorn'));
 
 	document.querySelector('.unicorn').remove();
-
 	t.is(prependElement(), await m('.unicorn'));
 });
