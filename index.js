912be43e052c398fa8eddd67ad7713ba22c9a095
Federico Brigante <github@bfred.it>
2019-05-24 02:54:26 +0800

    Avoid `alreadyFound` and use `options` destructuring

--- index.js
+++ index.js
@@ -5,24 +5,22 @@ const pDefer = require('p-defer');
 
 const cache = new ManyKeysMap();
 const elementReady = (selector, options) => {
-	const {target, stopOnDomReady} = {
-		target: document,
-		stopOnDomReady: true,
-		...options
-	};
+	const {
+		target = document,
+		stopOnDomReady = true
+	} = options;
 
 	const cacheKeys = [target, selector, stopOnDomReady];
 	const cachedPromise = cache.get(cacheKeys);
-
 	if (cachedPromise) {
 		return cachedPromise;
 	}
 
-	let alreadyFound = false;
 	let rafId;
-
 	const deferred = pDefer();
 	const {promise} = deferred;
+
+	cache.set(cacheKeys, promise);
 
 	const stop = () => {
 		cancelAnimationFrame(rafId);
@@ -40,16 +38,11 @@ const elementReady = (selector, options) => {
 
 		if (el) {
 			deferred.resolve(el);
-			alreadyFound = true;
 			stop();
 		} else {
 			rafId = requestAnimationFrame(check);
 		}
 	})();
-
-	if (!alreadyFound) {
-		cache.set(cacheKeys, promise);
-	}
 
 	return Object.assign(promise, {stop});
 };
