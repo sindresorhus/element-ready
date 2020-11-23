sindresorhus/element-ready

commit b02a075cc6dca33b2eeeda1ce102fd206aaef900

Have `stop` always handle the resolution

index.js
@@ -24,10 +24,10 @@ const elementReady = (selector, {
 
 	cache.set(cacheKeys, promise);
 
-	const stop = () => {
+	const stop = element => {
 		cancelAnimationFrame(rafId);
 		cache.delete(cacheKeys, promise);
-		deferred.resolve();
+		(element);
 	};
 
 	if (timeout !== Infinity) {
@@ -41,16 +41,14 @@ const elementReady = (selector, {
 		if (element) {
 			// If the document has finished loading, the elements are always "fully loaded"
 			if (!expectEntireElement || isDomReady(target)) {
-				deferred.resolve(element);
-				stop();
+				stop(element);
 				return;
 			}
 
 			let current = element;
 			do {
 				if (current.nextSibling) {
-					deferred.resolve(element);
-					stop();
+					stop(element);
 					return;
 				}
 

commit d229764188800a3850d0a96969a03eea11b4bbf3

Fix issue introduced by b02a075
const p = elementReady('.foo');
p.stop(6);
await p === 6

index.js
@@ -57,7 +57,7 @@ const elementReady = (selector, {
 		rafId = requestAnimationFrame(check);
 	})();
 
-	return Object.assign(promise, {stop});
+	return Object.assign(promise, {stop: () => stop()});
 };
 
 module.exports = elementReady;